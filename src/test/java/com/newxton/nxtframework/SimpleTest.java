9af057df1ad996f78cb350c284c34d5c26712fe2
Admin <18823207889@139.com>
2020-11-22 16:37:20 +0800

    更正轮播图片大小

---
+++ src/test/java/com/newxton/nxtframework/SimpleTest.java
@@ -0,0 +1,28 @@
+package com.newxton.nxtframework;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @author soyojo.earth@gmail.com
+ * @time 2020/9/15
+ * @address Shenzhen, China
+ */
+@RunWith(SpringRunner.class)
+@ActiveProfiles(profiles = "dev")
+@SpringBootTest
+public class SimpleTest {
+
+
+    @Test
+    public void helloTest() {
+
+        Assert.assertEquals(1, 1);
+
+    }
+
+}
