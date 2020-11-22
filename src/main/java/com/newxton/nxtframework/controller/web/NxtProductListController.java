26491d3f90eac181788dbb84287f52435ce85f86
Admin <18823207889@139.com>
2020-11-22 20:35:04 +0800

    产品列表&详情 controller 初始化

---
+++ src/main/java/com/newxton/nxtframework/controller/web/NxtProductListController.java
@@ -0,0 +1,46 @@
+package com.newxton.nxtframework.controller.web;
+
+import com.newxton.nxtframework.controller.api.front.NxtApiProductListController;
+import com.newxton.nxtframework.struct.NxtStructApiResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.mobile.device.Device;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.annotation.Resource;
+import java.util.Map;
+
+/**
+ * @author soyojo.earth@gmail.com
+ * @time 2020/11/22
+ * @address Shenzhen, China
+ */
+@Controller
+public class NxtProductListController {
+
+    private Logger logger = LoggerFactory.getLogger(NxtProductListController.class);
+
+    @Resource
+    NxtApiProductListController nxtApiProductListController;
+
+    @RequestMapping("/product/list")
+    public ModelAndView index(Device device, ModelAndView model) {
+
+//        if (device.isMobile()){
+//            model.setViewName("mobile/product_list");
+//            logger.info("移动端访客");
+//        }
+//        else {
+//            model.setViewName("pc/product_list");
+//            logger.info("PC端访客");
+//        }
+
+
+        return model;
+
+    }
+
+
+}
