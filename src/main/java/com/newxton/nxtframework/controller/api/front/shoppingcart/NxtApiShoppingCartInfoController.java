c37f6d13ede429a5ebf27e2ccdc738e6b2a234d8
Admin <18823207889@139.com>
2020-12-17 21:21:46 +0800

    bug fix

--- src/main/java/com/newxton/nxtframework/controller/api/front/shoppingcart/NxtApiShoppingCartInfoController.java
+++ src/main/java/com/newxton/nxtframework/controller/api/front/shoppingcart/NxtApiShoppingCartInfoController.java
@@ -3,9 +3,13 @@ package com.newxton.nxtframework.controller.api.front.shoppingcart;
 import com.alibaba.fastjson.JSONObject;
 import com.newxton.nxtframework.entity.NxtShoppingCart;
 import com.newxton.nxtframework.entity.NxtShoppingCartProduct;
+import com.newxton.nxtframework.exception.NxtException;
+import com.newxton.nxtframework.process.NxtProcessShoppingCart;
 import com.newxton.nxtframework.service.NxtShoppingCartProductService;
 import com.newxton.nxtframework.service.NxtShoppingCartService;
 import com.newxton.nxtframework.struct.NxtStructApiResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -22,24 +26,41 @@ import java.util.Map;
 @RestController
 public class NxtApiShoppingCartInfoController {
 
+    private Logger logger = LoggerFactory.getLogger(NxtApiShoppingCartInfoController.class);
+
     @Resource
     private NxtShoppingCartService nxtShoppingCartService;
 
     @Resource
     private NxtShoppingCartProductService nxtShoppingCartProductService;
 
+    @Resource
+    private NxtProcessShoppingCart nxtProcessShoppingCart;
+
     @RequestMapping(value = "/api/shopping_cart/info", method = RequestMethod.POST)
     public NxtStructApiResult exec(@RequestHeader(value = "user_id", required = false) Long userId,
                                    @RequestBody JSONObject jsonParam) {
+
+        String guestToken = jsonParam.getString("guestToken");
 
         NxtShoppingCart nxtShoppingCart;
 
         if (userId != null) {
             //查询已登录用户购物车
             nxtShoppingCart = nxtShoppingCartService.queryByUserId(userId);
+            //检查合并购物车
+            if (guestToken != null){
+                try {
+                    nxtProcessShoppingCart.mergeGuestShoppingCartToUser(guestToken, userId);
+                    nxtShoppingCart = nxtShoppingCartService.queryByUserId(userId);
+                }
+                catch (NxtException e) {
+                    //合并失败就不合并了
+                    logger.error("用户："+userId+" 购物车合并失败："+e.getNxtExecptionMessage());
+                }
+            }
         } else {
             //查询匿名用户购物车
-            String guestToken = jsonParam.getString("guestToken");
             nxtShoppingCart = nxtShoppingCartService.queryByToken(guestToken);
             if (nxtShoppingCart != null && nxtShoppingCart.getUserId() != null){
                 //已有归属的购物车，不能仅靠单独guestToken操作
