9965f09eefa662db688aa2478be98c99494099a9
Admin <18823207889@139.com>
2020-12-19 11:54:24 +0800

    bug fix

--- src/main/java/com/newxton/nxtframework/controller/api/front/NxtApiUserPwdResetController.java
+++ src/main/java/com/newxton/nxtframework/controller/api/front/NxtApiUserPwdResetController.java
@@ -84,6 +84,10 @@ public class NxtApiUserPwdResetController {
             return new NxtStructApiResult(54,"该用户已被加入黑名单");
         }
 
+        if (user.getIsAdmin() != null && user.getIsAdmin().equals(1)){
+            return new NxtStructApiResult(54,"管理员用户不可使用找回密码功能，请超级管理员修改");
+        }
+
         //验证成功，修改密码
         //创建salt和密码
         String saltNew = nxtUtilComponent.getRandomString(32);
